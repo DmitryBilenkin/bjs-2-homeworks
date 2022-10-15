ac0934d74f7a3d8dc3ae987ba7806acd0d9c0efd
Dmitriy Bilenkin <spayk94@gmail.com>
2022-10-15 19:41:44 +0300

    changed task.js (2.functions)

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,63 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
+  let min = arr[0];
+  let max = arr[0];
+  let sum = 0; 
+  let avg
 
-  // Ваш код
+  for (let i = 0; i < arr.length; i++){
+    if (arr[i] >= max){
+      max = arr[i];
+    } else if (arr[i] < min){
+      min = arr[i];
+    }
+    sum += arr[i];
+  }
+
+   avg = +(sum/arr.length).toFixed(2);
 
   return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
+  let sum = 0;
 
-  // Ваш код
+  for (let i = 0; i < arr.length; i++){
+    sum += arr[i];
+  }
 
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
+  let max = -Infinity;
 
-  // Ваш кода
-  // for ...
+  for (let i = 0; i < arrOfArr.length; i++){
+    let sum = func(arrOfArr[i]);
+    if (sum >= max){
+      max = sum;
+    }
+  }
   
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let max = arr[0];
+  let min = arr[0];
+  
+
+  for (let i = 0; i < arr.length; i++){
+    if (arr[i] >= max){
+      max = arr[i];
+    } else if (arr[i] < min){
+      min = arr[i];
+    }
+  }
+  
+  let difference = Math.abs(max - min);
+  return difference;
 }
+
