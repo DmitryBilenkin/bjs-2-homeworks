925ff4f94d87fdb395a1c58f726e09935a777046
Dmitriy Bilenkin <spayk94@gmail.com>
2023-01-05 23:14:36 +0300

    Redaction 2

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -9,63 +9,43 @@ function cachingDecoratorNew(func){
     let objectInCache = cache.find((item) => item.hash === hashObject.hash);
       if(objectInCache){
         let result = objectInCache.value;
-
-        console.log(`Из кэша: ${result}`);
-        console.log('Элементов в кэшэ:' + ' ' + cache.length);
-
         return `Из кэша: ${result}`;
-      } else {
-        hashObject.value = func(...args);
-        let result = hashObject.value;        
-        cache.push(hashObject);
-
-        console.log(`Вычисляем: ${result}`);
-
-            if(cache.length > 5){
-              cache.shift();
-              console.log('Хэш удален');
-            };
-
-            console.log('Элементов в кэшэ:' + ' ' + cache.length);
-
-        return `Вычисляем: ${result}`;
       }
+
+      hashObject.value = func(...args);
+      let result = hashObject.value;        
+      cache.push(hashObject);
+
+      if(cache.length > 5){
+        cache.shift();
+      };
+
+      return `Вычисляем: ${result}`;
+    }
   }
-}
+
 
 // Задача 2
 
-function debounceDecoratorNew(func, throttlDelay) {
-  let isTrottled = false;
-  console.log('флаг поднят при первом вызове троттл декоратора'); // для отладки  
-  
-  function wrapper (signalOrder, debuonceDelay){
-    wrapper.allCount += 1;
+function debounceDecoratorNew(func, debounceDecorDelay){
+  let timeoutId = null;  
 
-    if (isTrottled){
-      return  console.log(`флаг все еще опущен, вызов ${signalOrder} ${debuonceDelay}проигнорирован`); // для отладки
-    }
+  function wrapper (signalOrder, debounceDelay){
+    wrapper.allCount += 1;    
     
-    isTrottled = true;
-    console.log('флаг опущен'); // для отладки
+    if (timeoutId){
+      clearTimeout(timeoutId);
+    };
 
-    let result = func(signalOrder, debuonceDelay);
-    wrapper.count += 1;
-    console.log(`функция вызвана через ${debuonceDelay} и добавлена метка в count`); // для отладки
-    
-    setTimeout(() => {
-      isTrottled = false;
-      console.log('флаг поднят снова через' + ' ' + throttlDelay); // для отладки
-    }, throttlDelay);
-    console.log('запланированно поднятие флага через' + ' ' + throttlDelay); // для отладки
+    timeoutId = setTimeout(() => {
+      func(signalOrder, debounceDelay);  
+      timeoutId = null;
+      wrapper.count += 1;
+    }, debounceDecorDelay);
+  }
 
-    return result;
-  }  
-  
-  wrapper.count = 0;
-  wrapper.allCount = 0;
+    wrapper.count = 0;
+    wrapper.allCount = 0;
 
-  return wrapper;
-
-  
+    return wrapper;
 }
